df9a3643c4027f75da27fc8f46c5530640d97ac9
Ryan <wrosullivan88@gmail.com>
2022-10-12 19:26:54 -0500

    commit tree 281affc0938fc328315359a00bc5e378d42abc20

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+
+  const [inTheCart, setInTheCart] = useState(false)
+
+  const liClass = inTheCart ? "in-cart" : ""
+
+  const buttonText = inTheCart ? "Remove From Cart" : "Add to Cart"
+
+  function toggleInTheCart(){
+    setInTheCart(!inTheCart)
+  }
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button
+      className="add"
+      onClick = {toggleInTheCart}
+      >
+          {buttonText}
+      </button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState ('All')
+
+  function changeFilter(e) {
+    setSelectedCategory (e.target.value)
+  }
+
+  // function filterItems () {    this is if we don't use 'All' in line 6
+  //     if (selectedCategory === "All")
+  
+
+  const filteredItems = items.filter(item => item.category === selectedCategory || selectedCategory === "All") //29 to 
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange = {changeFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,7 +26,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => ( // change from items to filteredItems
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
